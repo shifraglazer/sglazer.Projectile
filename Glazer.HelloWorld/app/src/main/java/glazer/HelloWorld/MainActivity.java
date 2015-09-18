8c3c292b59adcd4beb8a166e6b220c3de3562bbc
a <A>
2015-09-17 22:25:36 -0400

    Hello world

---
+++ Glazer.HelloWorld/app/src/main/java/glazer/HelloWorld/MainActivity.java
@@ -0,0 +1,59 @@
+package glazer.HelloWorld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView text1;
+    private Button button;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text1 = (TextView) findViewById(R.id.text1);
+        button= (Button) findViewById(R.id.button1);
+        editText= (EditText) findViewById(R.id.editText1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                text1.setText(new Date().toString());
+            }
+        });
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        text1.setText(new Date().toString());
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
